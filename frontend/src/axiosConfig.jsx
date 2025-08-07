ad7530801a82ea4032515e49f679028d0f833809
kaitoozawa <kaitozaw@gmail.com>
2025-08-07 18:22:20 +1000

    Stop cicd

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  // baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://54.206.91.186', // live
+  baseURL: 'http://localhost:5001', // local
+  // baseURL: 'http://54.206.91.186', // live
   headers: { 'Content-Type': 'application/json' },
 });
 

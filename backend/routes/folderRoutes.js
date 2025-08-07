1a27c647bc56cefc44204188abea16000fce36e8
kaitoozawa <kaitozaw@gmail.com>
2025-08-07 22:40:53 +1000

    FILEBOX-26 Subtask 104-2: Modify backend/routes/ to add DELETE route for folder deletion

--- backend/routes/folderRoutes.js
+++ backend/routes/folderRoutes.js
@@ -1,11 +1,12 @@
 
 const express = require('express');
-const { getFolders, createFolder, updateFolder } = require('../controllers/folderController');
+const { getFolders, createFolder, updateFolder, deleteFolder } = require('../controllers/folderController');
 const { protect } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.get('/', protect, getFolders);
 router.post('/', protect, createFolder);
 router.put('/:id', protect, updateFolder);
+router.delete('/:id', protect, deleteFolder);
 
 module.exports = router;

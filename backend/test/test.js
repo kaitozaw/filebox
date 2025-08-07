860244dcf7cb5f9451f9747665a9d71511b046b9
kaitoozawa <kaitozaw@gmail.com>
2025-08-07 15:35:43 +1000

    Change cicd workflow

---
+++ backend/test/test.js
@@ -0,0 +1,13 @@
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const http = require('http');
+const app = require('../server'); 
+const connectDB = require('../config/db');
+const mongoose = require('mongoose');
+const sinon = require('sinon');
+const { expect } = chai;
+
+chai.use(chaiHttp);
+let server;
+let port;

b27ac63e4a5962375e346843ebff8a20f48eeb3d
kaitoozawa <kaitozaw@gmail.com>
2025-08-08 11:30:30 +1000

    FILEBOX-43 Subtask 204-2: Modify backend/routes/ to add PUT route for file rename

--- backend/routes/fileRoutes.js
+++ backend/routes/fileRoutes.js
@@ -1,11 +1,12 @@
 
 const express = require('express');
-const { listFilesInFolder, uploadFile, upload, downloadFile } = require('../controllers/fileController');
+const { listFilesInFolder, uploadFile, upload, downloadFile, renameFile } = require('../controllers/fileController');
 const { protect } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.get('/in-folder/:folderId', protect, listFilesInFolder);
 router.post('/in-folder/:folderId', protect, upload.single('file'), uploadFile);
 router.get('/:fileId/download', protect, downloadFile);
+router.put('/:fileId', protect, renameFile);
 
 module.exports = router;

4ad7ff5bcc5d1df85386f87a62a2831f5aba9c7c
kaitoozawa <kaitozaw@gmail.com>
2025-08-08 14:48:53 +1000

    FILEBOX-47 Subtask 301-1: Modify backend/controllers/ to implement public URL generation logic using UUID

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,13 +5,13 @@ const mongoose = require("mongoose");
 //mongoose.set('strictQuery', true);
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);  // Remove deprecated options
-    console.log("MongoDB connected successfully");
-  } catch (error) {
-    console.error("MongoDB connection error:", error.message);
-    process.exit(1);
-  }
+    try {
+        await mongoose.connect(process.env.MONGO_URI);  // Remove deprecated options
+        console.log("MongoDB connected successfully");
+    } catch (error) {
+        console.error("MongoDB connection error:", error.message);
+        process.exit(1);
+    }
 };
 
 module.exports = connectDB;
--- backend/routes/fileRoutes.js
+++ backend/routes/fileRoutes.js
@@ -1,12 +1,12 @@
 
 const express = require('express');
-const { listFilesInFolder, uploadFile, upload, downloadFile, renameFile, deleteFile } = require('../controllers/fileController');
+const { listFilesInFolder, downloadFile, uploadFile, upload, renameFile, deleteFile } = require('../controllers/fileController');
 const { protect } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.get('/in-folder/:folderId', protect, listFilesInFolder);
-router.post('/in-folder/:folderId', protect, upload.single('file'), uploadFile);
 router.get('/:fileId/download', protect, downloadFile);
+router.post('/in-folder/:folderId', protect, upload.single('file'), uploadFile);
 router.put('/:fileId', protect, renameFile);
 router.delete('/:fileId', protect, deleteFile);
 

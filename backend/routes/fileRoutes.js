18054322b5d1ec28c9c1354018c267e3a0cf355c
kaitoozawa <kaitozaw@gmail.com>
2025-08-08 11:21:42 +1000

    FILEBOX-35 Subtask 203-2: Modify backend/routes/ to add GET route for file download

--- backend/routes/fileRoutes.js
+++ backend/routes/fileRoutes.js
@@ -1,10 +1,11 @@
 
 const express = require('express');
-const { listFilesInFolder, uploadFile, upload } = require('../controllers/fileController');
+const { listFilesInFolder, uploadFile, upload, downloadFile } = require('../controllers/fileController');
 const { protect } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.get('/in-folder/:folderId', protect, listFilesInFolder);
 router.post('/in-folder/:folderId', protect, upload.single('file'), uploadFile);
+router.get('/:fileId/download', protect, downloadFile);
 
 module.exports = router;

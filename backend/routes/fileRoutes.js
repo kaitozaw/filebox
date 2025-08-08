0d81bcc79f8901d53d9ebad72254a04fb9b360f4
kaitoozawa <kaitozaw@gmail.com>
2025-08-08 11:10:06 +1000

    FILEBOX-31 Subtask 202-2: Modify backend/routes/ to add POST route for file upload

--- backend/routes/fileRoutes.js
+++ backend/routes/fileRoutes.js
@@ -1,9 +1,10 @@
 
 const express = require('express');
-const { listFilesInFolder } = require('../controllers/fileController');
+const { listFilesInFolder, uploadFile, upload } = require('../controllers/fileController');
 const { protect } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.get('/in-folder/:folderId', protect, listFilesInFolder);
+router.post('/in-folder/:folderId', protect, upload.single('file'), uploadFile);
 
 module.exports = router;

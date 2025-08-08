acae43b4e41fbc19c1b6a6e9e967642b79a8c69d
kaitoozawa <kaitozaw@gmail.com>
2025-08-08 11:09:35 +1000

    FILEBOX-30 Subtask 202-1: Modify backend/controllers/ to implement file upload logic using multer

--- backend/controllers/fileController.js
+++ backend/controllers/fileController.js
@@ -1,6 +1,8 @@
 
 const File = require('../models/File');
 const Folder = require('../models/Folder');
+const multer = require('multer');
+const path = require('path');
 
 // GET /api/files/in-folder/:folderId
 const listFilesInFolder = async (req, res) => {
@@ -23,4 +25,46 @@ const listFilesInFolder = async (req, res) => {
     }
 };
 
-module.exports = { listFilesInFolder };
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        const uniqueName = `${Date.now()}-${file.originalname}`;
+        cb(null, uniqueName);
+    },
+});
+
+const upload = multer({ storage: storage });
+
+// POST /api/files/in-folder/:folderId
+const uploadFile = async (req, res) => {
+    try {
+        const folder = await Folder.findById(req.params.folderId);
+    
+        if (!folder) {
+            return res.status(404).json({ message: 'Folder not found' });
+        }
+
+        if (folder.user.toString() !== req.user.id) {
+            return res.status(403).json({ message: 'Not authorized to upload to this folder' });
+        }
+
+        const file = req.file;
+
+        const newFile = await File.create({
+            user: req.user.id,
+            folder: folder._id,
+            name: file.originalname,
+            size: file.size,
+            mimetype: file.mimetype,
+            filePath: file.path,  
+        });
+
+        res.status(201).json(newFile);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+module.exports = { listFilesInFolder, uploadFile, upload };

b3af9bb51c05e4011c9f8ce37079c3353aeed773
Badley Teddy <bdlteddy1020@gmail.com>
2025-09-09 02:33:53 +1000

    modify folderroutes1

--- backend/routes/folderRoutes.js
+++ backend/routes/folderRoutes.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const { protect } = require('../middleware/authMiddleware');
 
-module.exports = ({ folderController }) => {
+module.exports = ({ folderController, zipController }) => {
     const router = express.Router();
 
     router.get('/', protect, folderController.getFolders.bind(folderController));
     router.post('/', protect, folderController.createFolder.bind(folderController));
     router.put('/:id', protect, folderController.updateFolder.bind(folderController));
     router.delete('/:id', protect, folderController.deleteFolder.bind(folderController));
-    router.get('/:id/zip', protect, folderController.downloadFolderZip.bind(folderController));
+    //router.get('/:id/zip', protect, folderController.downloadFolderZip.bind(folderController));
      // ZIP route: use zipController
     router.get('/:id/zip', protect, zipController.downloadFolderZip.bind(zipController));
 

8325d2a698268e69c007b73c73cae1fabb339312
kaitoozawa <kaitozaw@gmail.com>
2025-09-10 15:09:17 +1000

    fix login issue

--- backend/models/User.js
+++ backend/models/User.js
@@ -9,10 +9,4 @@ const userSchema = new mongoose.Schema({
     address: { type: String },
 });
 
-userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-});
-
 module.exports = mongoose.model('User', userSchema);

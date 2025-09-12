e5969759919f6ad48b38715cc72a3ddb5aeaa726
Takekami <tkyk.murakami@gmail.com>
2025-09-12 16:39:02 +1000

    FILEBOX-172 Subtask 602-2: Implement getRecent(req, res) in backend/controllers/RecentController.js

---
+++ backend/controllers/RecentController.js
@@ -0,0 +1,16 @@
+const BaseController = require('./BaseController');
+const RecentService = require('../services/RecentService');
+
+class RecentController extends BaseController {
+    constructor({ recentService = new RecentService() } = {}) {
+        super();
+        this.recentService = recentService;
+    }
+
+    async getRecent(req, res) {
+      try { return this.ok(res, await this.recentService.listByUser(req.user.id)); }
+      catch (err) { return this.handleError(res, err); }
+    }
+}
+
+module.exports = { RecentController };

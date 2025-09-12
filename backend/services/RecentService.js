76cfe1d8ea8da6fb4b7a52ca3291a37393cfb89b
Takekami <tkyk.murakami@gmail.com>
2025-09-12 20:41:57 +1000

    FILEBOX-171 Subtask 602-1: fix minor error on backend/services/RecentService.js

--- backend/services/RecentService.js
+++ backend/services/RecentService.js
@@ -2,7 +2,7 @@ const File = require('../models/File');
 
 class RecentService {
     async listByUser(userId, { limit = 20 } = {}) {
-      const lim = 20;
+      const lim = Math.min(100, Math.max(1, Number(limit) || 20));
       return File.find({
         user: userId,
         lastAccessedAt: { $ne: null }
@@ -14,4 +14,4 @@ class RecentService {
     }
     }
 
-    module.exports = RecentService;
+module.exports = RecentService;
